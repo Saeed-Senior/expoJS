2fa427a870e27ef7c9d78227cb4ed5aa45ddf2af
Saeed-Senior <nohchosaid@mail.ru>
2023-02-12 18:50:09 +0300

    DOM

--- js/script.js
+++ js/script.js
@@ -1,51 +1,20 @@
 'use strict';
 
+let ul = document.getElementById('ul');
 
-// 1.Task
-const arr_1 = [101, 202, 303, 404, 505];
-const arr_2 = [606, 707, 808, 909];
-const superArr = arr_1.concat(arr_2);
-console.log(superArr);
+ul.innerHTML = `
+<li>1</li>
+<li>2</li>
+<li>3</li>
+<li>4</li>
+`;
 
-
-// 2.Task
-console.log(Math.min(...superArr));
-
-
-// 3.Task
-const obj = {
-   width: 300,
-   height: 550,
-};
-console.log(obj);
-
-const obj2 = {...obj}
-obj2.area = function (){
-   return this.width * this.height;
-};
-console.log(obj2);
-console.log(obj2.area());
+console.log(ul);
 
 
-// 4.Task
-function num(...item){
-   let sum = item.reduce((s, i) => {return i + s}, 0)
-   console.log(sum);
-}
-num(5, 800, 10, 30, 5, 300);
 
 
-// 5.Task
-let number = 0;
-while (number < 3) {
-console.log(number);
-number++
-};
 
 
-// 6.Task
-for (let num = 1; num <= 10; num++){
-   console.log(num);
-};
 
 
